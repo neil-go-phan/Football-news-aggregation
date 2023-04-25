14b6e8c84700b97482fd48a523b0d39e1b13d8cb
Neil <neil.phan.goldenowl@gmail.com>
2023-04-25 15:13:26 +0700

    cors

--- backend/services/server/middlewares/cors.go
+++ backend/services/server/middlewares/cors.go
@@ -10,6 +10,7 @@ func Cors() gin.HandlerFunc {
 			"http://localhost:3000": true,
 			"http://localhost:8080": true,
 			"https://football-news-aggregation-j9rexyo0x-hung142-65.vercel.app": true,
+			"https://football-news-aggregation-9h8k5zhml-hung142-65.vercel.app":true,
 		}
 		if origin := c.Request.Header.Get("Origin"); allowList[origin] {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
