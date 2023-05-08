c3fbd2fab9f6d6156745a4fd6dbb8082a7c21876
Neil <neil.phan.goldenowl@gmail.com>
2023-05-08 15:07:10 +0700

    fix esline

--- frontend/src/components/contents/mainContents/schedule/scheduleContent.tsx
+++ frontend/src/components/contents/mainContents/schedule/scheduleContent.tsx
@@ -46,7 +46,11 @@ const ScheduleContent: FunctionComponent<Props> = ({ schedule }) => {
                       {match.club_1.name}
                       <Image
                         alt="club-logo"
-                        src={match.club_1.logo === "/images/CLB-MacDinh.svg" ? "https://bongda24h.vn/images/CLB-MacDinh.svg" : match.club_1.logo}
+                        src={
+                          match.club_1.logo === '/images/CLB-MacDinh.svg'
+                            ? 'https://bongda24h.vn/images/CLB-MacDinh.svg'
+                            : match.club_1.logo
+                        }
                         width={20}
                         height={20}
                       ></Image>
@@ -59,7 +63,11 @@ const ScheduleContent: FunctionComponent<Props> = ({ schedule }) => {
                     <p>
                       <Image
                         alt="club-logo"
-                        src={match.club_2.logo === "/images/CLB-MacDinh.svg" ? "https://bongda24h.vn/images/CLB-MacDinh.svg" : match.club_2.logo}
+                        src={
+                          match.club_2.logo === '/images/CLB-MacDinh.svg'
+                            ? 'https://bongda24h.vn/images/CLB-MacDinh.svg'
+                            : match.club_2.logo
+                        }
                         width={20}
                         height={20}
                       ></Image>
